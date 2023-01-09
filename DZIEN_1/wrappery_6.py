27e921ef9ea8f39948d395c1ad462b9f6136df75
albim72 <marcin.albiniak72@gmail.com>
2023-01-09 14:18:18 +0100

    Update wrappery_6.py

--- DZIEN_1/wrappery_6.py
+++ DZIEN_1/wrappery_6.py
@@ -10,8 +10,54 @@ def pomiarczasu(funkcja):
 
     return wrapper
 
+def sleep(funkcja):
+    def wrapper():
+        time.sleep(1)
+        return funkcja()
+    return wrapper
+
+
+@sleep
 @pomiarczasu
 def biglista():
     sum([i**3 for i in range(1000000)])
 
 biglista()
+
+
+lt = [i**3+100 for i in range(10000000)]
+
+@pomiarczasu
+def biglista_sec():
+    sum(lt)
+
+biglista_sec()
+
+#dekorator z użyciem funkcji magicznej __name__
+
+def debug(funkcja):
+    def wrapper(*args):
+        print(f'wołana funkcja: {funkcja.__name__}')
+        funkcja(*args)
+    return wrapper
+
+@debug
+def info(msg):
+    print(f"ważna informacja: {msg}")
+
+info("dokument 56466456")
+
+#repeater
+def repeater(n):
+    def wrapper(funkcja):
+        def inner(*args):
+            for i in range(n):
+                funkcja(*args)
+        return inner
+    return wrapper
+
+@repeater(n=6)
+def komunikat(k,n):
+    print(f'ważny komunikat nr {n}: {k}')
+
+komunikat("Ala ma kota",22)
