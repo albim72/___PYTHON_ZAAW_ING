12e3e7e48c5e725a3d470990df522cfd95501c36
albim72 <marcin.albiniak72@gmail.com>
2023-01-09 11:39:42 +0100

    Update pep8_2.py

--- DZIEN_1/pep8_2.py
+++ DZIEN_1/pep8_2.py
@@ -19,5 +19,22 @@ owoce = [
     ('malina',13.55),
     ('jabłko',2.33),
     ('mandarynka',8.01),
-    ('kiwi',2.67)
+    ('kiwi',2.67),
+    ('winogrono białe',12.44)
 ]
+
+enowoce = enumerate(owoce)
+print(list(enowoce))
+print(type(enowoce))
+
+print("__________cennik owoców__________")
+for i,(nazwa,cena) in enumerate(owoce):
+    print('#%d: %-15s = %6.2f zł' %(i,nazwa,cena))
+
+print("__________cennik owoców II__________")
+for i,(nazwa,cena) in enumerate(owoce):
+    print('#%d: %-15s = %6.2f zł' %(
+        i+1,
+        nazwa.title(),
+        round(cena,1)
+    ))
