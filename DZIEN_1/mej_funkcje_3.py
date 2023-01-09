33ab5cd49a52e299975fe7e73f81ad8d5dad0ee2
albim72 <marcin.albiniak72@gmail.com>
2023-01-09 12:04:23 +0100

    Update mej_funkcje_3.py

--- DZIEN_1/mej_funkcje_3.py
+++ DZIEN_1/mej_funkcje_3.py
@@ -12,3 +12,14 @@ def policz(a,b,c,y):
 
 print(policz(2,4,3,5))
 print(n)
+
+#przypadek 2
+
+def bx(n,m=1,k=4,f=2):
+    return math.sqrt(n+m) -k*f
+
+print(bx(12,7,8,3))
+print(bx(12,7,8))
+print(bx(12,7))
+print(bx(12))
+print(bx(5,6,f=7))
