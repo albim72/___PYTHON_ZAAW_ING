1018aed7f0ba89d1836dbedf54dbb918fa826464
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 15:36:32 +0100

    Create pojazd.py

---
+++ DZIEN_2/interfejs/pojazd.py
@@ -0,0 +1,17 @@
+from IPojazd import IPojazd
+
+class Pojazd(IPojazd):
+
+    def silnik(self, rodzaj_poj, kategoria, marka, model, rodzaj_silnika, poj):
+        return f"Pojazd -> {rodzaj_poj},{kategoria},marka: {marka},rodzaj silnika: {rodzaj_silnika}"
+
+    def trasa(self, start, koniec, odl, czas_przejazdu):
+        trasa = f"początek: {start}, koniec: {koniec},"
+        przej = f"odległość: {odl} km, czas przejazdu: {czas_przejazdu} h"
+        return trasa,przej
+
+    def spal_100(self, odl, litry):
+        return litry*100/odl
+
+    def koszt_przejazdu(self, odl, litry, cena_l):
+        return self.spal_100(odl,litry)*(odl/100)*cena_l
