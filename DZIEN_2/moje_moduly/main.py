8d23b9249a4713406778d15307baaf2362af11da
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 09:46:03 +0100

    Update main.py

--- DZIEN_2/moje_moduly/main.py
+++ DZIEN_2/moje_moduly/main.py
@@ -2,8 +2,8 @@
 #import dane as dn
 from dane import nr_filii as filia
 from dane import book as ksiazka
-
 from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+from klasy.kol_klasy import CDane
 
 print(filia)
 print(ksiazka)
@@ -11,3 +11,8 @@ print(ksiazka)
 print("_______________ czytanie kolekcji z użyciem funkcji _____________")
 czytaj_liste(filia)
 czytaj_slownik(ksiazka)
+
+print("_______________ czytanie kolekcji z użyciem metod obiektu _____________")
+cd = CDane(filia,ksiazka)
+cd.czyt_l()
+cd.czyt_s()
