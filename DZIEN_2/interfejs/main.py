470d023042b92d52cd4e53646b68aa57cd907af6
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 15:41:51 +0100

    Create main.py

---
+++ DZIEN_2/interfejs/main.py
@@ -0,0 +1,13 @@
+from pojazd import Pojazd
+
+p = Pojazd()
+
+print(p.silnik("samochód","osobowy","Opel","Vactra","diesel",1.9))
+tr,pr = p.trasa("Lublin","Warszawa",177,1.5)
+print(f"trasa: {tr}, {pr}")
+
+print(f"spalanie [l/100km]: {p.spal_100(173,19):.2f}")
+p.paliwo = 7.81
+print(p.paliwo)
+
+print(f'koszt przejazdu: {p.koszt_przejazdu(173,19,p.paliwo)} zł')
