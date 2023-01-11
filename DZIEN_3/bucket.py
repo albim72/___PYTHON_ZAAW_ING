cc2cea96e4aab7e9b3a35d2e8123069b296b7cec
albim72 <marcin.albiniak72@gmail.com>
2023-01-11 09:06:35 +0100

    Create bucket.py

---
+++ DZIEN_3/bucket.py
@@ -0,0 +1,14 @@
+from datetime import datetime,timedelta
+
+#krok 1
+class Bucket:
+    def __init__(self,period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.quota = 0
+
+    def __repr__(self):
+        return f'Bucket(quota = {self.quota})'
+
+bucket = Bucket(60)
+print(bucket)
