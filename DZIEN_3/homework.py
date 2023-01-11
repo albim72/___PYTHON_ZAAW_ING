663cf498786514612e2d7c7d6487b8dcdb508d38
albim72 <marcin.albiniak72@gmail.com>
2023-01-11 10:32:15 +0100

    Update homework.py

--- DZIEN_3/homework.py
+++ DZIEN_3/homework.py
@@ -1,5 +1,4 @@
-
-
+from weakref import WeakKeyDictionary
 
 class Homework:
     def __init__(self):
@@ -71,6 +70,43 @@ class Exam:
     math_grade = Grade()
     writing_grade = Grade()
     science_grade = Grade()
+
+f_exam = Exam()
+
+f_exam.writing_grade = 82
+f_exam.math_grade = 99
+f_exam.science_grade = 76
+print(f'recenzja: {f_exam.writing_grade}')
+print(f'obliczenia: {f_exam.math_grade}')
+print(f'analiza naukowa: {f_exam.science_grade}')
+
+s_exam = Exam()
+
+s_exam.math_grade = 22
+print(f'pierwszy egzamin - obliczenia: {f_exam.math_grade}')
+print(f'drugi egzamin - obliczenia: {s_exam.math_grade}')
+
+
+class NewGrade:
+    def __init__(self):
+        self._values = WeakKeyDictionary()
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return  self._values.get(instance,0)
+
+    def __set__(self, instance, value):
+        if not (0 <= value <= 100):
+            raise ValueError('ocena musi być wartością z zakresu 0 - 100')
+        self._values[instance] = value
+
+
+class Exam:
+    math_grade = NewGrade()
+    writing_grade = NewGrade()
+    science_grade = NewGrade()
+
 
 f_exam = Exam()
 
