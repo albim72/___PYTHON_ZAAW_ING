2b16c4741fafeac8a851cefd61574c43c6f5c10a
albim72 <marcin.albiniak72@gmail.com>
2023-01-11 09:14:46 +0100

    Update bucket.py

--- DZIEN_3/bucket.py
+++ DZIEN_3/bucket.py
@@ -21,3 +21,31 @@ def fill(bucket,amount):
         bucket.quota = 0
         bucket.reset_time = now
     bucket.quota += amount
+
+#krok3
+
+def deduct(bucket,amount):
+    now = datetime.now()
+    if (now-bucket.reset_time)>bucket.period_delta:
+        return False #wiadro nie zostało napełnione
+    if bucket.quota - amount < 0:
+        return False #wiadro zostało napełnione częściowo
+    bucket.quota -= amount
+    return True #wiadro zostało napełnione
+
+fill(bucket, 100)
+print(bucket)
+
+if deduct(bucket, 99):
+    print('potrzebne 99 jednostek danych')
+else:
+    print('nie ma 99 jednostek')
+
+print(bucket)
+
+if deduct(bucket, 3):
+    print('potrzebne 3 jednostki danych')
+else:
+    print('nie ma 3 jednostek')
+
+print(bucket)
