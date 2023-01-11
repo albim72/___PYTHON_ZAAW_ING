bb6f9844f585747007bac74369502a2eb57c5cd5
albim72 <marcin.albiniak72@gmail.com>
2023-01-11 09:24:21 +0100

    Update newbucket.py

--- DZIEN_3/newbucket.py
+++ DZIEN_3/newbucket.py
@@ -6,7 +6,24 @@ class NewBucket:
         self.reset_time = datetime.now()
         self.max_quota = 0
         self.quota_consumed = 0
-        
+
     def __repr__(self):
         return (f'NewBucket(max_quota = {self.max_quota}, '
                 f'quota_consumed = {self.quota_consumed}')
+
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+    
+    @quota.setter
+    def quota(self,amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
