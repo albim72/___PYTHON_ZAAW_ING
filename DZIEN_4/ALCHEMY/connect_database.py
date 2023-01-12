2696d914c5af6095ede1b29d97af1e79253d9a9f
albim72 <marcin.albiniak72@gmail.com>
2023-01-12 13:11:59 +0100

    Create connect_database.py

---
+++ DZIEN_4/ALCHEMY/connect_database.py
@@ -0,0 +1,6 @@
+import mysql.connector
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:abc123@localhost:3306/dbtestowa',echo=True)
