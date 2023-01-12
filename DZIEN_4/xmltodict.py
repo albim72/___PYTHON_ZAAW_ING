0a66e3372a13f5f9215af8856d6436a12ebd20d9
albim72 <marcin.albiniak72@gmail.com>
2023-01-12 12:11:48 +0100

    Create xmltodict.py

---
+++ DZIEN_4/xmltodict.py
@@ -0,0 +1,17 @@
+import xmltodict
+
+xml_data = """
+<student>
+    <id>F34354</id>
+    <name>
+        <firstname>Olga</firstname>
+        <lastname>Knap</lastname>
+    </name>
+    <email>knap@uczelnia.pl</email>
+    <class>inforamtyka</class>
+    <subjects>
+        <sub1>Sieci neuronowe</sub1>
+        <sub2>Programowanie obiektowe</sub2>
+    </subjects>
+</student>
+"""
