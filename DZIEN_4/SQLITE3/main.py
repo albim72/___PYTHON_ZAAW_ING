2ec295767c7bb7fee54906ac01cc86b9ab904b37
albim72 <marcin.albiniak72@gmail.com>
2023-01-12 14:28:32 +0100

    Update main.py

--- DZIEN_4/SQLITE3/main.py
+++ DZIEN_4/SQLITE3/main.py
@@ -1,7 +1,11 @@
 import sqlite3
 from sqlite3 import Error
 from conn import create_connection
+from create_tables import cr
+from insert_data import insert
+from select_data import select
 
 
 if __name__ == '__main__':
-    create_connection(r"C:\sqlite\db\specbaza.db")
+    #create_connection(r"C:\sqlite\db\specbaza.db")
+    select()
