5d2a07dc36d927f09b4eb5ce5cb02365424f953f
albim72 <marcin.albiniak72@gmail.com>
2023-01-12 14:50:55 +0100

    Create async_prime.py

---
+++ DZIEN_4/async_prime.py
@@ -0,0 +1,30 @@
+import time,concurrent.futures
+
+def find_prime_numbers_sum(minimum,maximum):
+    total = 0
+    for number in range(minimum,maximum+1):
+        count = 0
+        for i in range(2,(number//2+1)):
+            if number%i == 0:
+                count=count+1
+                break
+        if count == 0 and number!=1:
+            total = total + number
+
+    return total
+
+numbers = [(1,10000),(3,50000),(5000,100000),(4,900),(800,15000)]
+
+def run_heavy_function(params):
+    return find_prime_numbers_sum(*params)
+
+def main():
+    start_time = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+        result = executor.map(run_heavy_function,numbers)
+    print(list(result))
+    end_time = time.time()
+    print(f'czas całkowity wyznaczenia sum liczb pierwszych asynchronicznie -> {end_time - start_time:.2f} s')
+
+if __name__ == '__main__':
+    main()
