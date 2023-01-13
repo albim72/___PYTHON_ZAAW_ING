053e562f76316db1650fd7ddfc9943b0ba8d81fc
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 12:52:28 +0100

    Create main.py

---
+++ DZIEN_5/ABSTRACT_FACTORY/main.py
@@ -0,0 +1,18 @@
+from abstract import AbstractFactory
+from cf import ConcreteFactory1, ConcreteFactory2
+
+def client_code(factory:AbstractFactory) -> None:
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.useful_function_b()}")
+    print(f"{product_b.another_useful_function_b(product_a)}",end="")
+
+
+print("Klient: testowanie klienta w pierwszym typie fabryki")
+client_code(ConcreteFactory1())
+print("\n")
+
+print("Klient: testowanie klienta w drugim typie fabryki")
+client_code(ConcreteFactory2())
+print("\n")
