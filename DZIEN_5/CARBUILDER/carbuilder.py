0973c13a28892cdbcee13e05219a6669dde80e27
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 12:12:41 +0100

    Update carbuilder.py

--- DZIEN_5/CARBUILDER/carbuilder.py
+++ DZIEN_5/CARBUILDER/carbuilder.py
@@ -2,31 +2,44 @@ from abc import ABC,abstractmethod
 
 class Wheel:
     size = None
-    
+
 class Engine:
     horsepower = None
-    
+
 class Body:
     shape = None
-    
-    
+
+
 class Car:
     def __init__(self):
         self._wheels = list()
         self._engine = None
         self._body = None
-        
+
     def setBody(self,body):
         self._body = body
-        
+
     def setEngine(self,engine):
         self._engine = engine
         
     def attachWheel(self,wheel):
         self._wheels.append(wheel)
-        
+
     def specification(self):
         print(f'rodzaj pojazdu: {self._body.shape}')
         print(f'silnik [kM]: {self._engine.horsepower}')
         print(f'średnica koła: {self._wheels[0].size}')
         
+        
+#klasa Builder
+class Builder(ABC):
+    @abstractmethod
+    def getWheel(self):pass
+    
+    @abstractmethod
+    def getEngine(self):pass
+    
+    @abstractmethod
+    def getBody(self):pass
+    
+    
