a86eccfa61186ee1a879784ddce831c938668f59
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 09:05:46 +0100

    Create collections.py

---
+++ DZIEN_5/collections.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+seq1 = ['B','B','A','B','C','A','B','B','A','C']
+seq2 = {'A':13,'B':4,'C':2}
+
+print(Counter(seq1))
+print(Counter(seq2))
+
+print(Counter(A=5,B=7,C=1))
